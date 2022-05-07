20300f63e22957e6c96f1bf2e4f6b58e8da0391b
raghavchandel <87795692+raghavchandel@users.noreply.github.com>
2022-05-07 11:06:06 +0530

    Add files via upload

---
+++ hackermen/index.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--
+    <script src="js/jquery-1.8.2.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="Project.css">
+    <link rel="shortcut icon" href="img/teethicon.jpg">
+    <title>SANADOR || HOME</title>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+           <a class="navbar-brand" href="#" id="titlepic"></a>
+            <a class="navbar-brand" href="#" id="abc">SANADOR</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav" id="doright">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+
+                    <a class="nav-link" href="#aboutus">About</a>
+                    <a class="nav-link" href="#explore">Explore</a>
+                    <a class="nav-link" href="#appointments">Appointments</a>
+                    <a class="nav-link" href="#contactus">Contact Us</a>
+                    <a class="nav-link" href="#reachus">Reach Us</a>
+
+                </div>
+            </div>
+        </div>
+    </nav>
+
+<!---->
+   
+    <div class="container">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/bg2.jpeg" width="1240px" height="650px" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h1 id="abc">" Smile, you’re with us now "</h1>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="img/background.jpg" width="1240px" height="650px" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        
+                        <h3 id="xyz"> " Hair is the first thing and Teeth the second .<br> Hair and Teeth !!<br> A man got those two things, he’s got it all . "</h3>
+                    </div>
+                </div>
+
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+        <div class="row mt-5" id="aboutus">
+            <div class="col-md-12 bg-warning">
+                <center>About Us</center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card" style="border: 1px solid white;">
+                    <div class="card-body">
+                        <h5 class="card-title"></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"></h6>
+                        <p class="card-text">Sanador invites you as a patient! We utilize the most recent dental innovation to offer incredible dental consideration to give a charming, calm ordeal. For your benefit, our clinics offer nighttimes and Saturday arrangements just as electronic protection preparing for simple and fast installment.We pride ourselves on the consideration of our patients. We welcome patients all things considered and give benefits in both English and French. We enjoy incredible making your first dental involvement with us pleasant and keeping up long haul associations with all of our patients.
+                            Our theory for our clinics is the thing that we take a stab at ordinary. A family situated practice in an amicable and minding condition devoted to brilliance. We anticipate making cheerful, solid and splendid grins for you and your family in the years ahead.<br><br>At Sanador , our staff resembles a family. Every part is an esteemed advantage for our group and assumes an essential job in the patient experience.
+                            We try to separate ourselves as a medical procedure that gives exhaustive dentistry of the extremely most elevated quality that improves our patients lives. This is the reason we hope to grow our family with new colleagues who share our qualities and can enable us to make each patient grin!</p>
+                        <br><br><br><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="border: 1px solid white;">
+                    <img src="img/about.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-3" id="explore">
+            <div class="col-md-12 bg-warning">
+                <center>Explore</center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card" style="border: 1px solid white;">
+                    <div class="card-body">
+                        <h5 class="card-title"></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"></h6>
+                        <p class="card-text">If you want to see a positive change in your practice, you must consistently lead by example… be the change. It starts at the top. You set the tone for the day, make certain that it’s a happy & productive one. This includes any employee in a management position – if you have employees complaining about your office manager, take it seriously. A toxic office manager will destroy your practice over time, at the very least, cause a staff turnover nightmare. In fact, any employee that is toxic to your vision, to your team, and/or your patients…needs to go. It’s not easy to make the decision to terminate someone’s employment, yet absolutely necessary if you want to improve your practice. It’s been said that “what you allow you encourage.” Once you make the decision to no longer tolerate or allow the poor behavior and attitudes in your practice, you’ll breathe a huge sigh of relief.</p>
+                        <br><br><br><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="border: 1px solid white;">
+                    <img src="img/exp.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-3" id="appointments">
+            <div class="col-md-12 bg-warning">
+                <center>Appointments</center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-9">
+                <div class="card" style="border: 1px solid white;">
+                    <div class="card-body">
+                        <h5 class="card-title">Book an Appointment</h5>
+                        <h6 class="card-subtitle mb-2 text-muted"></h6>
+<!--------------------------------------form--------------------------->
+                        <form action="index-process.php" method="post" enctype="multipart/form-data">
+                            <div class="row">
+                            <div class="col-md-12 form-group">
+                                <label>Email-ID</label>
+                                <input type="email" name="uid" id="uid" class="form-control" required>
+                            </div>
+                            
+
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                <label>First Name</label>
+                                <input type="text" name="fname" id="fname" class="form-control" required>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Last Name</label>
+                                <input type="text" name="lname" id="lname" class="form-control" required>
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                <label>Contact no.</label>
+                                <input type="text" name="mobile" id="mobile" class="form-control" required>
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>City</label>
+                                <input type="text" name="city" id="city" class="form-control" required>
+                            </div>
+
+                        </div>
+                        <div class="row">
+                           <div class="col-md-6 form-group">
+                                        <label>Gender</label>
+                                        <select type="text" name="gend" id="gend" class="form-control form-select  mb-3" required>
+                                            <option value="none">Select</option>
+                                            <option value="male">Male</option>
+                                            <option value="female">Female</option>
+                                            <option value="other">Other</option>
+                                            
+                                        </select>
+                                    </div>
+                            
+                                <div class="col-md-6 form-group">
+                                        <label>Purpose of Visit</label>
+                                        <select type="text" name="purp" id="purp" class="form-control form-select  mb-3" required>
+                                            <option value="none">Select</option>
+                                            <option value="regularchkup">Regular Check-up</option>
+                                            <option value="oralc">Oral Cleaning</option>
+                                            <option value="cavitycheck">Check-up for Cavities</option>
+                                            <option value="sensitivity">Sensitivity</option>
+                                        </select>
+                                    </div>
+                            
+
+                        </div>
+                        <div clas="row">
+                            <center>
+                                 <div class="col-md-3 form-group ">
+                    <label>&nbsp;</label>
+                    <input type="submit"  class="form-control btn btn-danger">
+                </div>
+                            </center>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="border: 1px solid white;">
+                    <div class="card-body">
+                        <h5 class="card-title">Why Us?</h5>
+                        <br>
+                        <h6 class="card-subtitle mb-2 text-muted"></h6>
+                        <ul class="card-text">
+                            <li>24/7 Hours Available</li>
+                            <br>
+                            <li>Experienced Staff</li>
+                            <br>
+                            <li>Low Price and Fees</li>
+
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-3" id="contactus">
+            <div class="col-md-12 bg-warning">
+                <center>Contact Us</center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 offset-3">
+                <div class="card">
+                
+  
+  <div class="card-body">
+   <h5 class="card-title text-center">Developed by</h5>
+    <h6 class="card-text">
+        <table>
+           <tr>
+                <th>Developer : </th>
+                <td>HACKERMEN</td>
+            </tr>
+            
+            <tr>
+                <th>Contact us : </th>
+                <td>raghavchandel2003@gmail.com</td>
+            </tr>
+            
+            <tr>
+                <th>Institute : </th>
+                <td>Chitkara University</td>
+            </tr>
+            <tr>
+                <th>Guided by : </th>
+                <td>Chitkara University</td>
+            </tr>
+            
+            
+            
+        </table>
+    </h6>
+  </div>
+</div>
+            </div>
+            
+        </div>
+        <div class="row mt-3" id="reachus">
+            <div class="col-md-12 bg-warning">
+                <center>Reach Us</center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3428.19486376692!2d76.57366501513233!3d30.76910988162444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ffb140bd63e07%3A0x68591e334d17a988!2sChandigarh%20University!5e0!3m2!1sen!2sin!4v1626000819085!5m2!1sen!2sin" height="450" style="border:0; width:inherit;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </div>
+
+    </div>
+
+
+    <br><br><br><br>
+
+<center>
+    &copy;copyright <br>
+reserved <br>
+jsc kkp
+</center>
+
+<br><br><br><br>
+</body>
+
+</html>
---
+++ hackermen/mysql-connection.php
@@ -0,0 +1,12 @@
+<?php
+$dbcon=mysqli_connect("localhost","root","","sanador");
+
+$msg=mysqli_connect_error();
+/*
+if($msg=="")
+{
+    echo "connected";
+}
+*/
+?>
+
